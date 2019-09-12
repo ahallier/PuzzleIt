6bd3c328bcce2431e3b4f3bf570b70c54fc12458
ahallier <arhallier@gmail.com>
2019-09-11 20:58:01 -0500

    kinda have a grid of numbers

--- laugh-of-the-day/src/App.js
+++ laugh-of-the-day/src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Grid, Col, Row} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div class="container">
+      <div class="row">
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/1.png')}/>
+        </div>
+        <div style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/2.png')}/>
+        </div>
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/3.png')}/>
+        </div>
+      </div>
+      <div class="row">
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/4.png')}/>
+        </div>
+        <div style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/5.png')}/>
+        </div>
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/6.png')}/>
+        </div>
+      </div>
+      <div class="row">
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/7.png')}/>
+        </div>
+        <div style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/8.png')}/>
+        </div>
+        <div  style={{display: 'inline-block'}}>
+        <img style={{ height: '200px', width: '200px' }} src={require('./img/9.png')}/>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
+
+let placeholder = "./img/3.png";
+
+const renderImages = [...Array(3)].map((val) => {
+  return <Row className="show-grid">
+      <br />
+      <Col md={3}>
+          <img src={placeholder} />
+
+      </Col>
+      <Col md={3}>
+          <img src={placeholder} />
+
+      </Col>
+      <Col md={3}>
+          <img src={placeholder} />
+
+      </Col>
+
+  </Row>
+})
+
+const gridInstance = (
+  <Grid>
+      {renderImages}
+  </Grid>
+);
 
 export default App;
