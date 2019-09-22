d2cc90c42a62dc624d0b45d3dc08e27ad067997c
ahallier <arhallier@gmail.com>
2019-09-22 11:35:49 -0500

    add title, background color and centering

--- laugh-of-the-day/src/App.js
+++ laugh-of-the-day/src/App.js
@@ -1,75 +1,94 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Grid, Col, Row} from 'react-bootstrap';
 import './App.css';
 
-function App() {
+function Img(props) {
   return (
-    <div>
-    <div class="container">
-      <div class="row">
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/1.png')}/>
-        </div>
-        <div style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/2.png')}/>
-        </div>
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/3.png')}/>
-        </div>
-      </div>
-      <div class="row">
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/4.png')}/>
-        </div>
-        <div style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/5.png')}/>
-        </div>
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/6.png')}/>
-        </div>
-      </div>
-      <div class="row">
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/7.png')}/>
-        </div>
-        <div style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/8.png')}/>
-        </div>
-        <div  style={{display: 'inline-block'}}>
-        <img style={{ height: '200px', width: '200px' }} src={require('./img/9.png')}/>
-        </div>
-      </div>
-    </div>
+    <div  style={{display: 'inline-block'}}>
+      <img onClick={() => props.handleClick()} style={{ height: '200px', width: '200px' }} src={props.imgPath}/>
     </div>
   );
 }
 
-let placeholder = "./img/3.png";
+function Row(props) {
+  return (
+    <div class="row" style={{height: '200px'}}>
+      <Img handleClick={() => props.handleClick(props.configRow[0].value)} imgPath={props.configRow[0].path}/>
+      <Img handleClick={() => props.handleClick(props.configRow[1].value)} imgPath={props.configRow[1].path}/>
+      <Img handleClick={() => props.handleClick(props.configRow[2].value)} imgPath={props.configRow[2].path}/>
+    </div>
+  );
+}
 
-const renderImages = [...Array(3)].map((val) => {
-  return <Row className="show-grid">
-      <br />
-      <Col md={3}>
-          <img src={placeholder} />
+class App extends React.Component {
 
-      </Col>
-      <Col md={3}>
-          <img src={placeholder} />
+  constructor(props) {
+    super(props);
 
-      </Col>
-      <Col md={3}>
-          <img src={placeholder} />
+    const configuration = [];
 
-      </Col>
+    for (let j = 1; j <= 9; j++) {
+      configuration.push({value: j, path: 'img/'+j+'.png'});
+    }
 
-  </Row>
-})
+    this.state = {
+      active: null,
+      configuration: configuration,
+    };
 
-const gridInstance = (
-  <Grid>
-      {renderImages}
-  </Grid>
-);
+  }
+
+  swapActiveImageWithSelectedImage(imgVal) {
+    if (this.state.active === null) {
+      const config = this.state.configuration.slice();
+      this.setState({
+        configuration: config,
+        active: imgVal,
+      });
+    } else {
+      const config = this.state.configuration.slice();
+
+      const indexOfSelectedImg = this.getConfigLocal(imgVal);
+      const indexOfActiveImg = this.getConfigLocal(this.state.active);
+
+      const pathOfSelectedImg = config[indexOfSelectedImg].path;
+      const pathOfActiveImg = config[indexOfActiveImg].path;
+      
+      config[indexOfSelectedImg].path = pathOfActiveImg;
+      config[indexOfActiveImg].path = pathOfSelectedImg;
+
+      this.setState({
+        configuration: config,
+        active: null,
+      });
+    }
+  }
+
+  getConfigLocal(imgVal){
+    return this.state.configuration.findIndex((x) => x.value === imgVal);
+  }
+
+  render() {
+    return (
+      <html>
+        <header class="App-header">
+          <h1>Puzzle It</h1>
+        </header>
+        <body>
+          <div class="center">
+            <div class="container" style={{display: 'inline-block'}}>
+              <Row handleClick={(imgVal) => this.swapActiveImageWithSelectedImage(imgVal)} configRow={this.state.configuration.slice(0, 3)}/>
+              <Row handleClick={(imgVal) => this.swapActiveImageWithSelectedImage(imgVal)} configRow={this.state.configuration.slice(3, 6)}/>
+              <Row handleClick={(imgVal) => this.swapActiveImageWithSelectedImage(imgVal)} configRow={this.state.configuration.slice(6, 9)}/>
+            </div>
+          </div>
+        </body>
+      </html>
+      
+    );
+  }
+}
+
+
 
 export default App;
